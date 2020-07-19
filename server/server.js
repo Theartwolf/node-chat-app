0b63ffa8f75f99a7d0c66d6ce36db552823c4d88
Vivek Bansal <bansalvivek2323@gmail.com>
2020-07-20 00:39:45 +0530

    create generateMessage utility

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const socketIO = require('socket.io');
 const http = require('http');
+const { generateMessage } = require('./utils/message');
 
 const publicPath = path.join(__dirname , '../public');
 const port = process.env.PORT || 3000;
@@ -12,9 +13,15 @@ var io = socketIO(server);
 io.on('connection',(socket)=>{
     console.log('New user connected');
 
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to chat app'));
+
+    socket.broadcast.emit('newMessage',generateMessage('Admin', 'New User Joined'));
+
     socket.on('createMessage', (message)=>{
         console.log(message);
-        io.emit('newMessage',{
+        // io.emit('newMessage', generateMessage(message.from, meassage.text));
+        //broadcast.emit will not send message to the sender
+        socket.broadcast.emit('newMessage',{
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
