0b49220268cf96cd02c17712e93269c202c0fd5c
Vivek Bansal <bansalvivek2323@gmail.com>
2020-07-23 03:43:55 +0530

    Add css

--- public/js/index.js
+++ public/js/index.js
@@ -32,12 +32,13 @@ socket.on('newLocationMessage',function(message){
 
 jQuery('#message-form').on('submit', function(e){
     e.preventDefault();
+    var messageTextbox = jQuery('[name=message]');
 
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
-    }, ()=>{
-
+        text: messageTextbox.val()
+    }, function(){
+        messageTextbox.val('');
     })
 })
 
@@ -49,12 +50,17 @@ locationButton.on('click', function(){
         alert('Geolocation not supported by your browser');
     }
 
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function(position){
+        
+        locationButton.removeattr('disabled').text('Send location');    
         socket.emit('createLocationMessage',{
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         })
     }, function(){
+        locationButton.removeattr('disabled').text('Send location');
         alert('Unable to fetch location');
     })
 })
